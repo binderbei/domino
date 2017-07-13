d11a37079a5035c7697c389d4586951917bd6164
Matthias Brahm <matthias.brahm@netpioneer.de>
2017-07-13 16:22:13 +0200

    swagger, cors configuration

--- src/main/java/com/diva_e/DominoApplication.java
+++ src/main/java/com/diva_e/DominoApplication.java
@@ -6,9 +6,25 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DominoApplication {
+
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				super.addCorsMappings(registry);
+				registry.addMapping("/rest-api/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 
 	@Bean
 	public DominoDataCreator getDominoCreator() {
--- src/main/java/com/diva_e/controller/DominoRestController.java
+++ src/main/java/com/diva_e/controller/DominoRestController.java
@@ -5,6 +5,7 @@ import com.diva_e.services.DominoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +14,13 @@ import java.util.List;
  * Created by jbinder on 28.06.17.
  */
 @RestController
+@RequestMapping("rest-api")
 public class DominoRestController {
 
     @Autowired
     private DominoService dominoService;
 
-    @RequestMapping("/dominos/{moduleId}")
+    @RequestMapping(value = "/dominos/{moduleId}", method = RequestMethod.GET)
     public List<Domino> getModuleDominos(@PathVariable("moduleId") Integer moduleId) {
         return dominoService.getDominosForId(moduleId);
     }
